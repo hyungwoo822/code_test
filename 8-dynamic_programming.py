78ff72efcf9cbe73b00ea72edf746789a5c5fff3
hyungwoo <see0627@naver.com>
2025-05-22 14:05:59 +0900

    dp

---
+++ 8-dynamic_programming.py
@@ -0,0 +1,77 @@
+# import time
+# # fibonacci recur
+# def fibo_recur(x):
+#     if x == 1 or x == 2:
+#         return 1
+#     else :
+#         return fibo_recur(x-1) + fibo_recur(x-2)
+# start_time = time.time()
+# print(fibo_recur(40))
+# elapsed_time = time.time() - start_time
+# print(elapsed_time)
+
+# # fibonacci iter
+# d = [0] * 10001
+# def fibo_iter(x):
+#     d[0], d[1], d[2] = 0, 1, 1
+#     for i in range(3, x+1, 1):
+#         d[i] = d[i-1] + d[i-2]
+#     print(d[x])
+# start_time = time.time()
+# fibo_iter(40)
+# elapsed_time = time.time() - start_time
+# print(elapsed_time)
+
+# # Make 1
+# x = int(input())
+# d = [0] * 1001
+# d[1], d[2] = 0, 1
+# for i in range(3, x+1):
+#     d[i] = d[i-1] + 1
+#     if i % 2 == 0 :
+#         d[i] = min(d[i], d[i//2] + 1)
+#     if i % 3 == 0 :
+#         d[i] = min(d[i], d[i//3] + 1)
+#     if i % 5 == 0:
+#         d[i] = min(d[i], d[i//5] + 1)
+    
+# print(d[x])
+
+# Ant warrior
+x = int(input())
+array = list(map(int, input().split()))
+d = [0] * 100
+
+for i in range(1, x+1):
+    if i == 1:
+        d[i] = array[0]
+        continue
+    d[i] = max(d[i-1], d[i-2] + array[i-1])
+print(d[x])
+
+
+# # Tile optimization
+# x = int(input())
+# d = [0] * 10001
+# d[0], d[1], d[2], d[3] = 0, 1, 3, 5
+# for i in range(4, x+1):
+#     d[i] = 2 * d[i-2] + d[i-1]
+# print(d[x])
+
+# # Efficient currency construction
+# x, total = map(int, input().split())
+# data = []
+# for i in range(x):
+#     data.append(int(input()))
+# d = [10001] * 10001
+# d[0] = 0
+# for i in data:
+#     d[i] = 1
+# for i in range(total+1):
+#     for currency in data:
+#         if d[i-currency] != 10001:
+#             d[i] = min(d[i-currency]+1, d[i])
+# if d[total] == 10001:
+#     print(-1)
+# else :
+#     print(d[total])
