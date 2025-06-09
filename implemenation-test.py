fc2dcc8b7e17c7659b3fe3035d325fc96d14c5d7
admin <admin@adminui-MacBookPro.local>
2025-06-09 15:25:33 +0900

    ordering-test

--- implemenation-test.py
+++ implemenation-test.py
@@ -51,44 +51,48 @@
 
 
 
-### 10. lock and key
-def turn_clockwise(array):
-    n = len(array)
-    m = len(array)
-    result = [[0] * n for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n-i-1] = array[i][j]
-    return result
+# ### 10. lock and key
+# def turn_clockwise(array):
+#     n = len(array)
+#     m = len(array)
+#     result = [[0] * n for _ in range(m)]
+#     for i in range(n):
+#         for j in range(m):
+#             result[j][n-i-1] = array[i][j]
+#     return result
 
-def check(new_lock):
-    lock_leng = len(new_lock) // 3
-    for i in range(lock_leng, lock_leng * 2):
-        for j in range(lock_leng, lock_leng * 2):
-            if new_lock[i][j] != 1:
-                return False
-    return True
+# def check(new_lock):
+#     lock_leng = len(new_lock) // 3
+#     for i in range(lock_leng, lock_leng * 2):
+#         for j in range(lock_leng, lock_leng * 2):
+#             if new_lock[i][j] != 1:
+#                 return False
+#     return True
 
-def solution(key, lock):
-    n = len(lock)
-    m = len(key)
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
-    for i in range(n):
-        for j in range(n):
-            new_lock[i][j] = lock[i][j]
+# def solution(key, lock):
+#     n = len(lock)
+#     m = len(key)
+#     new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+#     for i in range(n):
+#         for j in range(n):
+#             new_lock[i][j] = lock[i][j]
     
-    for rotation in range(4):
-        key = turn_clockwise(key)
-        for x in range(n*2) :
-            for y in range(n*2):
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x+i][y+j] += key[i][j]
-                if check(new_lock) == True:
-                    return True
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x+i][y+j] -= key[i][j]
-    return False
+#     for rotation in range(4):
+#         key = turn_clockwise(key)
+#         for x in range(n*2) :
+#             for y in range(n*2):
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x+i][y+j] += key[i][j]x
+#                 if check(new_lock) == True:
+#                     return True
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x+i][y+j] -= key[i][j]
+#     return False
+
+
+### 11. Snake
+
 
 
---
+++ ordering.py
@@ -0,0 +1,84 @@
+### 23. Language, English, Mathematcis
+n = int(input())
+data = []
+for i in range(n):
+    data.append(input().split())
+
+data.sort(key=lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in data:
+    print(student[0])
+
+
+### 24. Antenna
+n = int(input())
+houses = list(map(int, input().split()))
+houses.sort()
+dist = 1e9
+result = None
+for i in range(len(houses)):
+    pos = houses[i]
+    temp = 0
+    for house in houses:
+        temp += abs(houses[i] - house)
+    if dist > temp : 
+        dist = temp
+        result = pos
+print(result)
+
+
+
+### 25. Failure Rate
+def solution(N, stages):
+    answer = []
+    failure = []
+    for i in range(1, N+1):
+        leng = 0
+        fail = 0
+        for stage in stages:
+            if stage >= i:
+                leng += 1
+                if i == stage :
+                    fail += 1
+        if leng == 0:
+            failure.append((i, 0))
+        else :
+            failure.append((i, fail / leng))
+    failure.sort(key = lambda x : x[1], reverse=True)
+    answer = [i[0] for i in failure]
+    return answer
+N = int(input())
+stages = list(map(int, input().split()))
+print(solution(N, stages))
+
+### 25.1. Failure Rate2
+def solution(N, stages):
+    answer = []
+    length = len(stages)
+    for i in range(1, N+1):
+        fail = stages.count(i)
+        if total == 0:
+            answer.append((i, 0))
+        else :
+            answer.append((i, fail / length))
+        length -= fail
+    answer.sort(key = lambda x : x[1], reverse=True)
+    answer = [i[0] for i in answer]
+    return answer
+
+
+### 26. Ordering Cards
+n = int(input())
+cards = []
+for i in range(n):
+    cards.append(int(input()))
+cards.sort()
+result = 0
+leng = len(cards)
+for i in range(leng):
+    if i == 0:
+        result += (leng - 1) * cards[i]
+    else :
+        result += (leng - 1) * cards[i]
+        leng -= 1
+print(result)
