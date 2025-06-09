ddb3b66a6c46743ef31a38a689827d3e56fbe0b9
admin <admin@adminui-MacBookPro.local>
2025-06-09 15:37:59 +0900

    order-test-2

--- ordering.py
+++ ordering.py
@@ -82,3 +82,23 @@ for i in range(leng):
         result += (leng - 1) * cards[i]
         leng -= 1
 print(result)
+
+### 26.1 Ordering cards : queue
+import heapq
+
+n = int(input())
+
+heap = []
+for i in range(n):
+    data = int(input())
+    heapq.heappush(heap, data)
+result = 0
+
+while len(heap) != 1 :
+    one = heapq.heappop(heap)
+    two = heapq.heapopp(heap)
+
+    sum_value = one + two
+    result += sum_value
+    heapq.heappush(heap, sum_value)
+print(result)
